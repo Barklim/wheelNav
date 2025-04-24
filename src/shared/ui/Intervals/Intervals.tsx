656ca6e7ba4ac2f1f5470252b494a2ec9af742c3
Barklim <46284548+Barklim@users.noreply.github.c>
2025-04-24 18:24:49 +0300

    feat: Intervals layout shifting
    feat: enhance Intervals component with improved layout and styles
    
    - Introduced intervalsInner and intervalsOuter classes for structured styling.
    - Added dummy span for spacing and improved color display for start and end intervals.

--- src/shared/ui/Intervals/Intervals.tsx
+++ src/shared/ui/Intervals/Intervals.tsx
@@ -1,50 +1,103 @@
-import React, { useEffect } from "react";
+import React, { useEffect} from "react";
 import gsap from "gsap";
 import { IntervalProps } from "./types";
 import styles from "./Intervals.module.scss";
 
-export const Intervals = ({ intervals, activeItem, duration, radius}: IntervalProps) => {
-    if (!intervals) {return null}
+export const Intervals = ({
+  intervals,
+  activeItem,
+  duration,
+  radius,
+}: IntervalProps) => {
+  if (!intervals) {
+    return null;
+  }
 
-    const [startInterval, setStartInterval] = React.useState({ value: intervals[0]?.start ?? 2000 })
-    const [endInterval, setEndInterval] = React.useState({ value: intervals[0]?.end ?? 2000 })
+  const [startInterval, setStartInterval] = React.useState({
+    value: intervals[0]?.start ?? 2000,
+  });
+  const [endInterval, setEndInterval] = React.useState({
+    value: intervals[0]?.end ?? 2000,
+  });
 
-    useEffect(() => {
-        const target = { value: startInterval.value };
-        if (intervals && intervals[activeItem]) {
-            const startPosition = intervals[activeItem].start
-            gsap.to(target, {
-                duration: duration*1,
-                value: startPosition,
-                roundProps: "value",
-                ease: 'circ.out',
-                onUpdate() {
-                    setStartInterval({ value: target.value });
-                }
-            });
-        }
-    }, [activeItem]);
+  const diameter = radius * 2;
+  const fontSize = `${radius * 0.74}px`;
 
-    useEffect(() => {
-        const target = { value: endInterval.value };
-        if (intervals && intervals[activeItem]) {
-            const endPosition = intervals[activeItem].end
-            gsap.to(target, {
-                duration: duration*1,
-                value: endPosition,
-                roundProps: "value",
-                ease: 'circ.out',
-                onUpdate() {
-                    setEndInterval({ value: target.value });
-                }
-            });
-        }
-    }, [activeItem]);
+  useEffect(() => {
+    const target = { value: startInterval.value };
+    if (intervals && intervals[activeItem]) {
+      const startPosition = intervals[activeItem].start;
+      gsap.to(target, {
+        duration: duration * 1,
+        value: startPosition,
+        roundProps: "value",
+        ease: "circ.out",
+        onUpdate() {
+          setStartInterval({ value: target.value });
+        },
+      });
+    }
+  }, [activeItem]);
 
-    return (
-        <div className={styles.intervals} style={{top: radius, position: 'relative', fontSize: `${radius - 65}px`}}>
-            <span className={styles.intervals__left}>{startInterval.value}</span>
-            <span className={styles.intervals__right}>{endInterval.value}</span>
-        </div>
-    )
-}
+  useEffect(() => {
+    const target = { value: endInterval.value };
+    if (intervals && intervals[activeItem]) {
+      const endPosition = intervals[activeItem].end;
+      gsap.to(target, {
+        duration: duration * 1,
+        value: endPosition,
+        roundProps: "value",
+        ease: "circ.out",
+        onUpdate() {
+          setEndInterval({ value: target.value });
+        },
+      });
+    }
+  }, [activeItem]);
+
+  return (
+    <>
+      <div
+        className={styles.intervalsOuter}
+        style={{
+          top: radius,
+          position: "relative",
+          fontSize: fontSize,
+        }}
+      >
+        <span className={styles.startColor}>
+          <span className={styles.a}>
+            {String(startInterval.value).slice(0, 2)}
+          </span>
+        </span>
+        <span
+          className={styles.dummy}
+          style={{ width: `${diameter}px` }}
+        ></span>
+        <span className={styles.endColor}>
+          <span className={styles.a}>
+            {String(endInterval.value).slice(-2)}
+          </span>
+        </span>
+      </div>
+
+      <div
+        className={styles.intervalsInner}
+        style={{
+          top: radius,
+          position: "relative",
+          fontSize: fontSize,
+          width: `${diameter}px`,
+        }}
+      >
+        <span className={styles.startColor}>
+          <span>{String(startInterval.value).slice(-2)}</span>
+        </span>
+
+        <span className={styles.endColor}>
+          <span>{String(endInterval.value).slice(0, 2)}</span>
+        </span>
+      </div>
+    </>
+  );
+};

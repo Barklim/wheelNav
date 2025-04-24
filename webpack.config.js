37ad3e663b31943f1ac066eaa2682d3d25c212ad
Barklim <46284548+Barklim@users.noreply.github.c>
2025-04-24 19:03:39 +0300

    feat: webpack config for local and for deploy
    feat: update webpack configuration and styles
    
    - Enhanced sass-loader options to include paths for better SCSS imports.
    - Updated index.scss to use 'PT Sans' font for improved typography.

--- webpack.config.js
+++ webpack.config.js
@@ -2,93 +2,95 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-let isDev = true;
-
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    publicPath: '/'
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    alias: {
-      '@fonts': path.resolve(__dirname, 'src/shared/assets/fonts')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.module\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]__[hash:base64:5]',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: "./src/index.tsx",
+    output: {
+      filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+      publicPath: isProduction ? '/wheelNav/' : '/'
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+      alias: {
+        '@fonts': path.resolve(__dirname, 'src/shared/assets/fonts')
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.module\.s[ac]ss$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]__[local]__[hash:base64:5]',
+                },
               },
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: {
-                includePaths: [path.resolve(__dirname, 'src')]
+            {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  includePaths: [path.resolve(__dirname, 'src')]
+                }
               }
             }
-          }
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        exclude: /\.module\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: {
-                includePaths: [path.resolve(__dirname, 'src')]
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          exclude: /\.module\.s[ac]ss$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  includePaths: [path.resolve(__dirname, 'src')]
+                }
               }
             }
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][ext]'
           }
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name][ext]'
-        }
-      },
-    ],
-  },
-  
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css'
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
+        },
+      ],
     },
-    port: 3000,
-    open: true,
-    hot: true,
-  },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: isProduction ? 'css/[name].[contenthash:8].css' : 'css/[name].css',
+        chunkFilename: isProduction ? 'css/[name].[contenthash:8].css' : 'css/[name].css'
+      })
+    ],
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, "dist"),
+      },
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true
+    },
+  };
 };
